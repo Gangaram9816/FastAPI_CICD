ff989b5d7dbb74828453390aba18c190d73bbb50
Gangaram9816 <147996755+Gangaram9816@users.noreply.github.com>
2024-02-07 15:48:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello FastAPI App"}
 
 if __name__=='__main__':
     uvicorn.run(app)
